443a2f3a6c2d64f1de72b125ff3601865f99065e
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-07-17 21:48:10 -0300

    Support formatting things at runtime when they are not known at compile-time

--- src/hyperscript.nim
+++ src/hyperscript.nim
@@ -2,11 +2,13 @@ import macros
 import sequtils
 import strformat
 import strtabs
+import strutils
 import xmltree
+
+const RunnableNodes = {nnkIdent, nnkSym, nnkDotExpr} + CallNodes
 
 func hxml(xs: varargs[NimNode]): XmlNode
 
-const RunnableNodes = {nnkIdent, nnkSym, nnkDotExpr} + CallNodes
 
 func getTagElement(name: NimNode): XmlNode =
   case name.kind:
@@ -100,14 +102,75 @@ func hxml(xs: varargs[NimNode]): XmlNode =
   for x in xs[1..^1]:
     result.addGeneric(x)
 
+func transform(node: XmlNode): NimNode =
+  case node.kind:
+  of xnText:
+    # TODO: here we should check if the text is in fact a Nim expression. The generated code should check the type of the expression using `when`.
+    result = newCall(
+      ident"newText",
+      newTree(
+        nnkCallStrLit,
+        ident"&",
+        newLit(node.text),
+      ),
+    )
+  of xnElement:
+    let children = newTree(nnkBracket)
+    for child in node:
+      children.add transform(child)
+    let attrs = if not isNil(node.attrs):
+      let a = newTree(nnkTableConstr)
+      for k, v in node.attrs:
+        if k.startsWith("on"):
+          debugEcho("event handlers are ignored when not compiling to JavaScript: " & k)
+        else:
+          a.add newTree(
+            nnkExprColonExpr,
+            newTree(
+              nnkCallStrLit,
+              ident"&",
+              newLit(k),
+            ),
+            newTree(
+              nnkCallStrLit,
+              ident"&",
+              newLit(v),
+            ),
+          )
+      newCall(
+        ident"toXmlAttributes",
+        a,
+      )
+    else:
+      newNilLit()
+    result = newCall(
+      ident"newXmlTree",
+      newTree(
+        nnkCallStrLit,
+        ident"&",
+        newLit(node.tag),
+      ),
+      children,
+      attrs,
+    )
+  else:
+    raise newException(ValueError, "unsupported node kind: " & $node.kind)
+
 macro h(xs: varargs[untyped]): untyped =
-  let el = hxml(xs.toSeq)
-  debugEcho el
+  result = transform hxml(xs.toSeq)
+  debugEcho repr result
+
 
 when isMainModule:
+  let version = 3
   var count = 0
-  h("div", "id": "app", "class": "app", "data-version": version,
-    h("button", "+", onclick: increment),
+  func increment(e: auto) =
+    count += 1
+  func decrement(e: auto) =
+    count -= 1
+
+  echo h("div", "id": "app", "class": "app", "data-version": version,
+    h("button", "+", "onclick": increment),
     h("div", [count]),
-    h("button", "-", onclick: decrement),
+    h("button", "-", "onclick": decrement),
   )
